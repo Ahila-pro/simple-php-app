0a3fc23ee087f80c2e4d70e4a88b54803c350183
Ahila-pro <ahilaasokan2025@gmail.com>
2025-10-22 13:33:06 +0530

    index file changed after add city field

--- index.php
+++ index.php
@@ -7,13 +7,16 @@
     <h2>Enter Your Name</h2>
     <form method="post" action="">
         <input type="text" name="username" placeholder="Type your name" required>
+        <input type="text" name="cityname" placeholder="Type your city" required>
         <button type="submit">Submit</button>
     </form>
 
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = htmlspecialchars($_POST['username']); // sanitize input
+        $name = htmlspecialchars($_POST['username']);
+        $cname = htmlspecialchars($_POST['cityname']); // sanitize input
         echo "<h3>Hello, " . $name . "!</h3>";
+        echo "<h3>Welcome, " . $cname . "!</h3>";
     }
     ?>
 </body>
