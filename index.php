d56791a2b02403c8ed1b88a7e8b78599648c255a
Ahila-pro <ahilaasokan2025@gmail.com>
2025-10-22 22:07:55 +0530

    cname1 added

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <div>
             <input type="text" name="username" placeholder="Type your name" required>
             <input type="text" name="cityname" placeholder="Type your city" required>
+            <input type="text" name="cityname1" placeholder="Type your city" required>
         </div>
         <br>
         <div>
@@ -19,9 +20,11 @@
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = htmlspecialchars($_POST['username']);
-        $cname = htmlspecialchars($_POST['cityname']); // sanitize input
+        $cname = htmlspecialchars($_POST['cityname']); 
+        $cname1 = htmlspecialchars($_POST['cityname1']);// sanitize input
         echo "<h3>Hello, " . $name . "!</h3>";
         echo "<h3>Welcome to " . $cname . "!</h3>";
+        echo "<h3>Welcome to " . $cname1 . "!</h3>";
     }
     ?>
 </body>
